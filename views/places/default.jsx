8e3f9c83d8a55c44e9a80d12fe15091f040b64d4
mvictn <imvictor78@gmail.com>
2022-09-18 10:38:51 -0700

    navbar with style

--- views/places/default.jsx
+++ views/places/default.jsx
@@ -5,10 +5,23 @@ function Def (html){
         <html>
             <head>
                 <title>Title</title>
-                <link rel="stylesheet" href="style.css" />
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossOrigin="anonymous"/>
+                <link rel="stylesheet" href="style.css"/>
             </head>
             <body>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href='/'>Home</a>
+                        </li>
+                        <li>
+                            <a href='/places'>Places</a>
+                        </li>
+                        <li>
+                            <a href='/places/new'>Add Place</a>
+                        </li>
+                    </ul>
+                </nav>
                 {html.children}
             </body>
         </html>
---
+++ views/places/home.jsx
@@ -0,0 +1,23 @@
+const React = require('react')
+const Def = require('./default')
+
+function home(){
+    return(
+      <Def>
+        <main>
+          <h1>HOME</h1>
+          <div>
+              <img src="/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" width ="50%"/>
+            <div>
+              Photo by <a href="https://unsplash.com/@brookelark?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Brooke Lark</a> on <a href="https://unsplash.com/s/photos/food-free?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+            </div>
+          </div>
+          <a href="/places">
+            <button className="btn btn-primary">Places Page</button>
+          </a>
+        </main>
+      </Def>
+    )
+}
+
+module.exports = home
