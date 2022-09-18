05bca0c6803c4f9c8a92f11942cc154e5810fcaa
mvictn <imvictor78@gmail.com>
2022-09-17 23:14:01 -0700

    new places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -22,10 +22,20 @@ router.get('/', (req,res) => {
     res.render('places/index',{places})
 })
 
-router.post('/', (req,res) =>{
-    console.log(req.body)
-    res.send('POST/places')
+router.post('/', (req, res) => {
+  if (!req.body.pic) {
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
+
 
 
 
